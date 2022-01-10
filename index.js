d3f615084be1f2fa69ab89cc7ad51cbf05353c90
Bruno Pinho <brunopinho@outlook.com>
2022-01-10 12:33:54 -0300

    iniciado requisito 1. feito junto com o wesley maia. dando erro

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+const express = require('express');
+
+const { createProductController } = require('./controllers/products.controller');
+const { errorMiddleware } = require('./middlewares/errorMiddleware');
+
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.post('/products', createProductController);
+
+app.use(errorMiddleware);
+
+app.listen(port, () => console.log('Ouvindo a porta 3000'));
